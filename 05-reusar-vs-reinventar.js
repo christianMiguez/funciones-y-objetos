6ad4151b78e37be8a0d335a9787056bbf058fc13
Rodrigo Juliani <srodriki@gmail.com>
2018-06-16 17:14:56 -0300

    Correccion de commit anterior

--- 05-reusar-vs-reinventar.js
+++ 05-reusar-vs-reinventar.js
@@ -21,3 +21,23 @@ a = 6;
 b = 7;
 
 console.log(a + b);
+
+
+/**
+ * Necesitamos crear un programa que nos imprima
+ * en consola la suma entre 2 y 3, 5 y 8, 6 y 7 
+ */
+
+/**
+ * Creamos una funcion suma y la reusamos
+ */
+
+function suma(a, b) {
+    console.log(a + b);
+}
+
+suma(2, 3);
+suma(5, 8);
+suma(6, 7);
+
+
